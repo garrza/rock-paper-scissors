223afc4105f3616b123947166e12894d1f6d8bfc
Ramiro Garza <garza.ramirov@gmail.com>
2023-03-19 14:32:41 -0600

    Creative CSS incorpotation, class disable and enable

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let computerWins = 0;
 
 // computer choice function
 function getComputerChoice() {
-    const pick = ["rock", "paper", "scissors"];
-    return pick[Math.floor(Math.random() * 3)];
+    let pickOne = ["rock", "paper", "scissors"];
+    let randomOne = pickOne[Math.floor(Math.random() * pickOne.length)];
+    return randomOne;
 }
 
 // global variables for both selections, no scope
@@ -14,21 +15,32 @@ let computerSelection;
 
 // play round function, recording score and returning winner
 function playRound(playerSelection, computerSelection) {
-    winner = null;
-    if (playerSelection[0] === "r" && computerSelection[0] === "s") {
-        return winner = "player";
-    }
-    else if (playerSelection[0] === "p" && computerSelection[0] === "r") {
-        return winner = "player";
-    }
-    else if (playerSelection[0] === "s" && computerSelection[0] === "p") {
-        return winner = "player";
-    }
-    else if (playerSelection[0] === computerSelection[0]) {
-        return winner = "tie";
-    }
-    else {
-        return winner = "computer";
+    if (playerSelection === computerSelection) {
+        return `It is a tie! You both picked ${playerSelection}.`
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            computerWins++;
+            return "You lose! Paper beats rock!";
+        } else if (computerSelection === "scissors") {
+            playerWins++;
+            return "You win! Rock beats scissors!";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            playerWins++;
+            return "You win! Paper beats rock!";
+        } else if (computerSelection === "scissors") {
+            computerWins++;
+            return "You lose! Scissors beats paper!";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            computerWins++;
+            return "You lose! Rock beats scissors!";
+        } else if (computerSelection === "paper") {
+            playerWins++;
+            return "You win! Scissors beats paper!";
+        }
     }
 }
 
@@ -43,6 +55,69 @@ function playGame(pick) {
     computerSelection = getComputerChoice();
 
     resultRound.textContent = playRound(pick, computerSelection);
+
+    humanScore.textContent = playerWins;
+    robotScore.textContent = computerWins;
+
+    if (playerWins == 5) {
+        resultGame.textContent = "Victory is yours!";
+        resultRound.textContent = ''
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        playAgain.disabled = false;
+        rockBtn.className = "optionsAfterGame";
+        paperBtn.className = "optionsAfterGame";
+        scissorsBtn.className = "optionsAfterGame";
+        playAgain.className = "optionsAgain"
+    } else if (computerWins == 5) {
+        resultGame.textContent = "You lost to a computer, noob!";
+        resultRound.textContent = ''
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        playAgain.disabled = false;
+        rockBtn.className = "optionsAfterGame";
+        paperBtn.className = "optionsAfterGame";
+        scissorsBtn.className = "optionsAfterGame";
+        playAgain.className = "optionsAgain"
+    }
 }
 
-console.log(playGame());
+
+// restart button
+playAgain.addEventListener('click', function () {
+
+    playerWins = 0;
+    computerWins = 0;
+    resultGame.textContent = ''
+    resultRound.textContent = ''
+    humanScore.textContent = playerWins;
+    robotScore.textContent = computerWins;
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    playAgain.disabled = true;
+    rockBtn.className = "options";
+    paperBtn.className = "options";
+    scissorsBtn.className = "options";
+    playAgain.className = "optionsAfterGame"
+})
+
+// functioning the buttons
+
+rockBtn.addEventListener("click", function () {
+    playGame("rock");
+});
+
+
+paperBtn.addEventListener("click", function () {
+    playGame("paper");
+});
+
+
+scissorsBtn.addEventListener("click", function () {
+    playGame("scissors");
+});
+
+console.log()
