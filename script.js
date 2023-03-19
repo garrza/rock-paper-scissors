068d615737888272de4451e7d272e1cfc035b5bf
Ramiro Garza <garza.ramirov@gmail.com>
2023-03-18 19:22:02 -0600

    New CSS, first implemantation of eventListeners and selectors

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+// variables for storing scores
+let playerWins = 0;
+let computerWins = 0;
+
+// computer choice function
 function getComputerChoice() {
     const pick = ["rock", "paper", "scissors"];
     return pick[Math.floor(Math.random() * 3)];
 }
 
+// global variables for both selections, no scope
+let playerSelection;
+let computerSelection;
+
+// play round function, recording score and returning winner
 function playRound(playerSelection, computerSelection) {
     winner = null;
-    if (playerSelection.charAt(0) === "r" && computerSelection.charAt(0) === "s") {
+    if (playerSelection[0] === "r" && computerSelection[0] === "s") {
         return winner = "player";
     }
-    else if (playerSelection.charAt(0) === "p" && computerSelection.charAt(0) === "r") {
+    else if (playerSelection[0] === "p" && computerSelection[0] === "r") {
         return winner = "player";
     }
-    else if (playerSelection.charAt(0) === "s" && computerSelection.charAt(0) === "p") {
+    else if (playerSelection[0] === "s" && computerSelection[0] === "p") {
         return winner = "player";
     }
-    else if (playerSelection.charAt(0) === computerSelection.charAt(0)) {
+    else if (playerSelection[0] === computerSelection[0]) {
         return winner = "tie";
     }
     else {
@@ -22,27 +32,17 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function playGame() {
-    let playerWins = 0;
-    let computerWins = 0;
-    while (playerWins < 3 && computerWins < 3) {
-        const playerSelection = prompt("Rock, paper, or scissors?");
-        const computerSelection = getComputerChoice();
-        winner = playRound(playerSelection, computerSelection);
-        if (winner == "player") {
-            console.log("You won!");
-            playerWins += 1;
-        }
-        else if (winner == "computer") {
-            console.log(`You lost, the computer chose ${computerSelection}!`);
-            computerWins += 1;
-        }
-        else if (winner == "tie") {
-            console.log("It is a tie!");
-        }
-    }
-    finalWinner = (playerWins === 3) ? "player" : "computer";
-    return `The final winner is the ${finalWinner}, with three wins!`;
+// selectors for different user action buttons
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const playAgain = document.querySelector('#playAgain');
+
+playAgain.disabled = true;
+function playGame(pick) {
+    computerSelection = getComputerChoice();
+
+    resultRound.textContent = playRound(pick, computerSelection);
 }
 
 console.log(playGame());
